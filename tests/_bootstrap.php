6595d57a61b19e640493507c61d424fec14d54c8
Christoffer Lindqvist <christoffer.lindqvist@nordsoftware.com>
2015-02-03 13:59:50 +0200

    Add feature to notify Nosto when an account is deleted

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,6 +4,7 @@
 // Pre-load all sdk classes.
 require_once(dirname(__FILE__) . '/../src/config.inc.php');
 
-// Configure API and OAuth client to use Mock server when testing.
+// Configure API, Web Hooks, and OAuth client to use Mock server when testing.
 NostoApiRequest::$baseUrl = 'http://localhost:3000';
 NostoOAuthClient::$baseUrl = 'http://localhost:3000';
+NostoHttpRequest::$baseUrl = 'http://localhost:3000';
