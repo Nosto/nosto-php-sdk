870b80cedda0cb0d4097f77518b9925ca5a37b9e
Hannu Pölönen <hannu.polonen@nosto.com>
2016-01-29 15:24:36 +0200

    Move exceptions into their respective files

--- src/NostoInvalidArgumentException.php
+++ src/NostoInvalidArgumentException.php
@@ -34,13 +34,6 @@
  */
 
 /**
- * Nosto exception class for all errors within the sdk.
- */
-class NostoException extends Exception
-{
-}
-
-/**
  * Nosto exception class for invalid arguments.
  */
 class NostoInvalidArgumentException extends NostoException
