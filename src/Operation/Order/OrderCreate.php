509d93206eeffdedd1a394eeee01de86b85e66ff
Olsi Qose <olsi.qose@nosto.com>
2019-06-27 19:13:41 +0300

    Start implementation of order create GraphQL

---
+++ src/Operation/Order/OrderCreate.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Copyright (c) 2019, Nosto Solutions Ltd
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Nosto Solutions Ltd <contact@nosto.com>
+ * @copyright 2019 Nosto Solutions Ltd
+ * @license http://opensource.org/licenses/BSD-3-Clause BSD 3-Clause
+ *
+ */
+
+namespace Nosto\Operation\Order;
+
+class OrderCreate extends AbstractOrderCreate
+{
+
+    public function getQuery()
+    {
+        $query = <<<QUERY
+            {
+                "query":"mutation {
+                    placeOrder(by:BY_REF, id: "514421fce84abcb61bd45241", params: {
+                        customer: {
+                            firstName: {$this->customer->getFirstName()}
+                            lastName: {$this->customer->getLastName()}
+                            email: {$this->customer->getEmail()}
+                            marketingPermission: {$this->customer->getMarketingPermission()}
+                        }
+                        order: {
+                            number: {$this->orderNumber}
+                            orderStatus: "paid"
+                            paymentProvider: "klarna"
+                            ref: "0010"
+                            purchasedItems: [
+                                {
+                                    name: "Shoe"
+                                    productId: "1"
+                                    skuId: "11"
+                                    priceCurrencyCode: "EUR"
+                                    unitPrice: 22.43
+                                    quantity: 1
+                                }
+                            ]
+                        }
+                    }) {
+                    id
+                   }
+                }"
+            }
+QUERY;
+
+
+    }
+}
