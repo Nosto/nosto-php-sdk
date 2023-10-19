cdc4517b12d4be8d55690e9cae249448584abd58
Tobias Graml <tobias.graml@nosto.com>
2023-10-19 13:50:23 +0200

    Add classes for search requests

---
+++ src/Result/Graphql/Search/SearchResultHandler.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Nosto\Result\Graphql\Search;
+
+use Nosto\Result\Graphql\GraphQLResultHandler;
+use stdClass;
+
+class SearchResultHandler extends GraphQLResultHandler
+{
+    protected function parseQueryResult(stdClass $stdClass)
+    {
+        return $stdClass;
+    }
+}
