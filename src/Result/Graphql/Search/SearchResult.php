5192663e61b450acad90f775fe9f01e29d6408f0
vladimir.chagorovski <vladimir.chagorovski@nosto.com>
2026-04-10 20:02:46 +0300

    ADS-5342 fix failing pipeline

--- src/Result/Graphql/Search/SearchResult.php
+++ src/Result/Graphql/Search/SearchResult.php
@@ -55,7 +55,7 @@ class SearchResult
      */
     public function getSearchType()
     {
-        return $this->products?->getSearchType();
+        return $this->products ? $this->products->getSearchType() : null;
     }
 
     /**
